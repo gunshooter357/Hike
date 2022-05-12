ce0ac20b0d901d78008a129c3e552c5613578461
shotomorisaki <103382369+shotomorisaki@users.noreply.github.com>
2022-05-11 23:39:21 -0700

    Update MemberList.cpp

--- Hike/MemberList.cpp
+++ Hike/MemberList.cpp
@@ -1,1 +1,13 @@
+/*
+    Team Morisaki
+
+    Claude, Austin // member 1
+    Morisaki, Shoto // member 2 
+    Mustafa Atakan Tan // member 3 
+
+    Spring 2022
+    CS A250 - C++ 2
+    Project: Hiking in the US    
+*/
+
 #include "MemberList.h"
