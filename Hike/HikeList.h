c5120814203d909c7d686b605b77744e6cf5c1cb
shotomorisaki <103382369+shotomorisaki@users.noreply.github.com>
2022-05-11 23:36:39 -0700

    Update HikeList.h

--- Hike/HikeList.h
+++ Hike/HikeList.h
@@ -1,3 +1,15 @@
+/*
+    Team Morisaki
+
+    Claude, Austin // member 1
+    Morisaki, Shoto // member 2 
+    Mustafa Atakan Tan // member 3 
+
+    Spring 2022
+    CS A250 - C++ 2
+    Project: Hiking in the US    
+*/
+
 #ifndef HIKE_H
 #define HIKEList_H
 #include "Hike.h"
