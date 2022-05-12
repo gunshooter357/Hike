671d8e56f6b1c7059d67f750ddfb426764095cf6
shotomorisaki <103382369+shotomorisaki@users.noreply.github.com>
2022-05-11 23:39:49 -0700

    Update Source.cpp

--- Hike/Source.cpp
+++ Hike/Source.cpp
@@ -1,3 +1,15 @@
+/*
+    Team Morisaki
+
+    Claude, Austin // member 1
+    Morisaki, Shoto // member 2 
+    Mustafa Atakan Tan // member 3 
+
+    Spring 2022
+    CS A250 - C++ 2
+    Project: Hiking in the US    
+*/
+
 #include <iostream>
 #include "HikeList.h"
 #include "Hike.h"
