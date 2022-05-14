f0a56105e57855d8b9aaf1712faa34d8e7dc5d3c
Mustafa Tan <60370747+atakan682@users.noreply.github.com>
2022-05-14 07:55:56 -0700

    Update MemberList.h

--- Hike/MemberList.h
+++ Hike/MemberList.h
@@ -1,17 +1,28 @@
 /*
     Team Morisaki
-
     Claude, Austin // member 1
-    Morisaki, Shoto // member 2 
-    Mustafa Atakan Tan // member 3 
-
+    Morisaki, Shoto // member 2
+    Mustafa Atakan Tan // member 3
     Spring 2022
     CS A250 - C++ 2
-    Project: Hiking in the US    
+    Project: Hiking in the US
 */
 
-#pragma once
+#define MEMBERList_H
+#include "Hike.h"
+#include <string>
+#include <map>
+#include <set>
+#include "Member.h"
 class MemberList
 {
+public: 
+    MemberList();
+    
+    void addMember(std::string firstName, std::string lastName);
+    void addMember(std::string firstName, std::string lastName, int points);
+    int getLastID();
+    int getPoints(int memberId);
+private:
+   std::set<Member> *ptrToSet;
 };
-
