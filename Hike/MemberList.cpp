656714ee20b25c0f8c313c2f7f034cb3c1582b6e
Mustafa Tan <60370747+atakan682@users.noreply.github.com>
2022-05-14 07:54:57 -0700

    Update MemberList.cpp

--- Hike/MemberList.cpp
+++ Hike/MemberList.cpp
@@ -1,13 +1,28 @@
 /*
     Team Morisaki
-
     Claude, Austin // member 1
-    Morisaki, Shoto // member 2 
-    Mustafa Atakan Tan // member 3 
-
+    Morisaki, Shoto // member 2
+    Mustafa Atakan Tan // member 3
     Spring 2022
     CS A250 - C++ 2
-    Project: Hiking in the US    
+    Project: Hiking in the US
 */
 
 #include "MemberList.h"
+#include "Hike.h"
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+MemberList::MemberList() {
+
+}
+void MemberList::addMember(string firstName, string lastName)
+{
+    ptrToSet->insert(make_pair(firstName, lastName));
+}
+void MemberList::addMember(string firstName, string lastName, int points)
+{
+    ptrToSet->insert(make_pair(firstName, lastName));
+
+}
