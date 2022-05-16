3983b34e972721a5a23b6c3e88b763cced2e6234
shotomorisaki <103382369+shotomorisaki@users.noreply.github.com>
2022-05-16 14:57:12 -0700

    Update Reservations.cpp

--- Hike/Reservations.cpp
+++ Hike/Reservations.cpp
@@ -1,4 +1,43 @@
 #include "Reservations.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+void Reservations()
+{
+    
+}
+
+int addReservation(int memberID, string namHike)
+{
+    if(first == nullptr)
+    {
+        first = new Node(memberID, nullptr, nullptr);
+        last = first;
+        ++count;
+        return reservNum;
+    }
+    else
+    {
+        last->setNext(new Node(memberID, last, nullptr));    
+        last = last->getNext();
+        ++count;
+        reservNum++;
+        return reservNum;
+    }
+}
+
+int * findReservation(int reservNum)
+{
+    
+     for(int i = 0; i < sizeOfList; i++)
+     {
+         if(DoublyLinked[i] == reservNum)
+        {
+            return 
+        }
+     }
+}
 
 void cancelReservation(int reservation_number,DLLptr &L)
 {
