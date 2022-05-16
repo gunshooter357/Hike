65a732f4c8030e80bf8d43c4c5c9805af3256822
shotomorisaki <103382369+shotomorisaki@users.noreply.github.com>
2022-05-16 14:57:30 -0700

    Update Reservations.h

--- Hike/Reservations.h
+++ Hike/Reservations.h
@@ -1,6 +1,23 @@
-#pragma once
-class Reservations
-{
+#ifndef RESERVATIONS_H
+#define RESERVATIONS_H
+#include <string>
 
+class Hike
+{
+    public:
+        Reservations();
+        int addReservation(int memberID, std::string namHike);
+        void cancelReservation(int reservation_number,DLLptr &L);
+
+    private:
+        int reservNum = 50001;
+        int memberId;
+        std::string namHike;
+        Node* next;
+        Node* prev;
+        int * findReservation(int reservNum);
 };
 
+
+
+#endif
