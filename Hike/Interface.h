d737207751e56f3cd2ae58eb3d15faeb3176f04e
Mustafa Tan <60370747+atakan682@users.noreply.github.com>
2022-05-17 10:07:25 -0700

    Update Interface.h

--- Hike/Interface.h
+++ Hike/Interface.h
@@ -1,1 +1,21 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+#include <string>
 
+/*
+Note that all parameter objects 
+(not primitive values) should be passed by reference 
+(because they are objects), but not as constants
+*/
+
+class Interface:public Hike
+{
+    public:
+        void chooseByDifficulty(HikeList& HList, MemberList& MList, Reservations& Reserve);
+        void chooseByPrice(HikeList& HList, MemberList& MList, Reservations& Reserve);
+        int askIfMember(MemberList& MList);
+        int addNewMember(MemberList& MList);
+
+    private:
+
+}
